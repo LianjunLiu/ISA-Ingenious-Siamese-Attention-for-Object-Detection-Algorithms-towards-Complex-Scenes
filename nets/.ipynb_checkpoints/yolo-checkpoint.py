9e65dc3007c780c8e817f9f2cb2d509768089691
LianjunLiu <liulianjun_2022@163.com>
2023-11-21 11:16:36 +0800

    V1.0

---
+++ nets/.ipynb_checkpoints/yolo-checkpoint.py
@@ -0,0 +1,363 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+
+from nets.CSPdarknet import darknet53
+
+from torchsummary import summary
+
+
+
+# 通道注意力机制
+class ChannelAttentionModule(nn.Module):
+    def __init__(self, channel, ratio=16):
+        super(ChannelAttentionModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.shared_MLP = nn.Sequential(
+            nn.Conv2d(channel, channel // ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(channel // ratio, channel, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avgout = self.shared_MLP(self.avg_pool(x))
+        # print(avgout.shape)
+        maxout = self.shared_MLP(self.max_pool(x))
+        return self.sigmoid(avgout + maxout)
+
+
+# 空间注意力机制
+class SpatialAttentionModule(nn.Module):
+    def __init__(self):
+        super(SpatialAttentionModule, self).__init__()
+        self.conv2d = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avgout = torch.mean(x, dim=1, keepdim=True)
+        maxout, _ = torch.max(x, dim=1, keepdim=True)
+        out = torch.cat([avgout, maxout], dim=1)
+        out = self.sigmoid(self.conv2d(out))
+        return out
+
+
+# CBAM 模块
+class CBAM(nn.Module):
+    def __init__(self, channel):
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttentionModule(channel)
+        self.spatial_attention = SpatialAttentionModule()
+
+    def forward(self, x):
+        out = self.channel_attention(x) * x
+        # print('outchannels:{}'.format(out.shape))
+        out = self.spatial_attention(out) * out
+        return out
+
+    
+'''
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+
+
+# TransformerLayer
+class TransformerLayer(nn.Module):
+    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
+    def __init__(self, c, num_heads):
+        super().__init__()
+        self.q = nn.Linear(c, c, bias=False)
+        self.k = nn.Linear(c, c, bias=False)
+        self.v = nn.Linear(c, c, bias=False)
+        self.ma = nn.MultiheadAttention(embed_dim=c, num_heads=num_heads)
+        self.fc1 = nn.Linear(c, c, bias=False)
+        self.fc2 = nn.Linear(c, c, bias=False)
+
+    def forward(self, x):
+        x = self.ma(self.q(x), self.k(x), self.v(x))[0] + x
+        x = self.fc2(self.fc1(x)) + x
+        return x
+
+
+# Transformer 模块
+class TransformerBlock(nn.Module):
+    # Vision Transformer https://arxiv.org/abs/2010.11929
+    def __init__(self, c1, c2, num_heads, num_layers):
+        super().__init__()
+        self.conv = None
+        if c1 != c2:
+            self.conv = Conv(c1, c2)
+        self.linear = nn.Linear(c2, c2)  # learnable position embedding
+        self.tr = nn.Sequential(*[TransformerLayer(c2, num_heads) for _ in range(num_layers)])
+        self.c2 = c2
+
+    def forward(self, x):
+        if self.conv is not None:
+            x = self.conv(x)
+        b, _, w, h = x.shape
+        p = x.flatten(2).unsqueeze(0).transpose(0, 3).squeeze(3)
+        return self.tr(p + self.linear(p)).unsqueeze(3).transpose(0, 3).reshape(b, self.c2, w, h)
+'''
+
+
+def conv2d(filter_in, filter_out, kernel_size, stride=1):
+    pad = (kernel_size - 1) // 2 if kernel_size else 0
+    return nn.Sequential(OrderedDict([
+        ("conv", nn.Conv2d(filter_in, filter_out, kernel_size=kernel_size, stride=stride, padding=pad, bias=False)),
+        ("bn", nn.BatchNorm2d(filter_out)),
+        ("relu", nn.LeakyReLU(0.1)),
+    ]))
+
+
+#---------------------------------------------------#
+#   SPP结构，利用不同大小的池化核进行池化
+#   池化后堆叠
+#---------------------------------------------------#
+class SpatialPyramidPooling(nn.Module):
+    def __init__(self, pool_sizes=[5, 9, 13]):
+        super(SpatialPyramidPooling, self).__init__()
+
+        self.maxpools = nn.ModuleList([nn.MaxPool2d(pool_size, 1, pool_size//2) for pool_size in pool_sizes])
+
+    def forward(self, x):
+        features = [maxpool(x) for maxpool in self.maxpools[::-1]]
+        features = torch.cat(features + [x], dim=1)
+
+        return features
+
+
+#---------------------------------------------------#
+#   卷积 + 上采样
+#---------------------------------------------------#
+class Upsample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Upsample, self).__init__()
+
+        self.upsample = nn.Sequential(
+            conv2d(in_channels, out_channels, 1),
+            nn.Upsample(scale_factor=2, mode='nearest')
+        )
+
+    def forward(self, x,):
+        x = self.upsample(x)
+        return x
+
+
+#---------------------------------------------------#
+#   三次卷积块
+#---------------------------------------------------#
+def make_three_conv(filters_list, in_filters):
+    m = nn.Sequential(
+        conv2d(in_filters, filters_list[0], 1),
+        conv2d(filters_list[0], filters_list[1], 3),
+        conv2d(filters_list[1], filters_list[0], 1),
+    )
+    return m
+
+
+#---------------------------------------------------#
+#   五次卷积块
+#---------------------------------------------------#
+def make_five_conv(filters_list, in_filters):
+    m = nn.Sequential(
+        conv2d(in_filters, filters_list[0], 1),
+        conv2d(filters_list[0], filters_list[1], 3),
+        conv2d(filters_list[1], filters_list[0], 1),
+        conv2d(filters_list[0], filters_list[1], 3),
+        conv2d(filters_list[1], filters_list[0], 1),
+    )
+    return m
+
+
+#---------------------------------------------------#
+#   最后获得yolov4的输出
+#---------------------------------------------------#
+def yolo_head(filters_list, in_filters):
+    m = nn.Sequential(
+        conv2d(in_filters, filters_list[0], 3),
+        nn.Conv2d(filters_list[0], filters_list[1], 1),
+    )
+    return m
+
+
+#---------------------------------------------------#
+#   yolo_body
+#---------------------------------------------------#
+class YoloBody(nn.Module):
+    def __init__(self, anchors_mask, num_classes, pretrained = False):
+        super(YoloBody, self).__init__()
+        #---------------------------------------------------#   
+        #   生成CSPdarknet53的主干模型
+        #   获得三个有效特征层，他们的shape分别是：
+        #   52,52,256
+        #   26,26,512
+        #   13,13,1024
+        #---------------------------------------------------#
+        self.backbone   = darknet53(pretrained)
+
+        # 加入第 1 个CBAM模块
+        self.CBAM1 = CBAM(channel=1024)
+
+        # 加入第 1 个 TransformerLayer
+        # self.TransformerBlock1 = TransformerBlock(c1=1024, c2=1024, num_heads=8, num_layers=2)
+
+        self.conv1      = make_three_conv([512,1024],1024)
+        self.SPP        = SpatialPyramidPooling()
+        self.conv2      = make_three_conv([512,1024],2048)
+
+        # 加入第 2 个CBAM模块
+        self.CBAM2 = CBAM(channel=512)
+
+        # 加入第 2 个 TransformerLayer
+        # self.TransformerBlock2 = TransformerBlock(c1=512, c2=512, num_heads=8, num_layers=2)
+
+        self.upsample1          = Upsample(512,256)
+        self.conv_for_P4        = conv2d(512,256,1)
+        self.make_five_conv1    = make_five_conv([256, 512],512)
+
+        # 加入第 3 个CBAM模块
+        self.CBAM3 = CBAM(channel=256)
+
+        # 加入第 3 个 TransformerLayer
+        # self.TransformerBlock3 = TransformerBlock(c1=256, c2=256, num_heads=8, num_layers=2)
+
+        self.upsample2          = Upsample(256,128)
+        self.conv_for_P3        = conv2d(256,128,1)
+        self.make_five_conv2    = make_five_conv([128, 256],256)
+
+        # 加入第 4 个CBAM模块
+        self.CBAM4 = CBAM(channel=128)
+
+        # 加入第 4 个 TransformerLayer
+        # self.TransformerBlock4 = TransformerBlock(c1=128, c2=128, num_heads=8, num_layers=2)
+
+        # 3*(5+num_classes) = 3*(5+20) = 3*(4+1+20)=75
+        self.yolo_head3         = yolo_head([256, len(anchors_mask[0]) * (5 + num_classes)],128)
+
+        self.down_sample1       = conv2d(128,256,3,stride=2)
+        self.make_five_conv3    = make_five_conv([256, 512],512)
+
+        # 加入第 5 个CBAM模块
+        self.CBAM5 = CBAM(channel=256)
+
+        # 加入第 5 个 TransformerLayer
+        # self.TransformerBlock5 = TransformerBlock(c1=256, c2=256, num_heads=8, num_layers=2)
+
+        # 3*(5+num_classes) = 3*(5+20) = 3*(4+1+20)=75
+        self.yolo_head2         = yolo_head([512, len(anchors_mask[1]) * (5 + num_classes)],256)
+
+        self.down_sample2       = conv2d(256,512,3,stride=2)
+        self.make_five_conv4    = make_five_conv([512, 1024],1024)
+
+        # 加入第 6 个CBAM模块
+        self.CBAM6 = CBAM(channel=512)
+
+        # 加入第 6 个 TransformerLayer
+        # self.TransformerBlock6 = TransformerBlock(c1=512, c2=512, num_heads=8, num_layers=2)
+
+        # 3*(5+num_classes)=3*(5+20)=3*(4+1+20)=75
+        self.yolo_head1         = yolo_head([1024, len(anchors_mask[2]) * (5 + num_classes)],512)
+
+
+    def forward(self, x):
+        #  backbone
+        x2, x1, x0 = self.backbone(x)
+
+        x0 = self.CBAM1(x0)
+        # x0 = self.TransformerBlock1(x0)
+
+        # 13,13,1024 -> 13,13,512 -> 13,13,1024 -> 13,13,512 -> 13,13,2048 
+        P5 = self.conv1(x0)
+        P5 = self.SPP(P5)
+        # 13,13,2048 -> 13,13,512 -> 13,13,1024 -> 13,13,512
+        P5 = self.conv2(P5)
+
+        P5 = self.CBAM2(P5)
+        # P5 = self.TransformerBlock2(P5)
+
+        # 13,13,512 -> 13,13,256 -> 26,26,256
+        P5_upsample = self.upsample1(P5)
+        # 26,26,512 -> 26,26,256
+        P4 = self.conv_for_P4(x1)
+        # 26,26,256 + 26,26,256 -> 26,26,512
+        P4 = torch.cat([P4,P5_upsample],axis=1)
+        # 26,26,512 -> 26,26,256 -> 26,26,512 -> 26,26,256 -> 26,26,512 -> 26,26,256
+        P4 = self.make_five_conv1(P4)
+        P4 = self.CBAM3(P4)
+        # P4 = self.TransformerBlock3(P4)
+
+        # 26,26,256 -> 26,26,128 -> 52,52,128
+        P4_upsample = self.upsample2(P4)
+        # 52,52,256 -> 52,52,128
+        P3 = self.conv_for_P3(x2)
+        # 52,52,128 + 52,52,128 -> 52,52,256
+        P3 = torch.cat([P3,P4_upsample],axis=1)
+        # 52,52,256 -> 52,52,128 -> 52,52,256 -> 52,52,128 -> 52,52,256 -> 52,52,128
+        P3 = self.make_five_conv2(P3)
+        P3 = self.CBAM4(P3)
+        # P3 = self.TransformerBlock4(P3)
+
+        # 52,52,128 -> 26,26,256
+        P3_downsample = self.down_sample1(P3)
+        # 26,26,256 + 26,26,256 -> 26,26,512
+        P4 = torch.cat([P3_downsample,P4],axis=1)
+        # 26,26,512 -> 26,26,256 -> 26,26,512 -> 26,26,256 -> 26,26,512 -> 26,26,256
+        P4 = self.make_five_conv3(P4)
+        P4 = self.CBAM5(P4)
+        # P4 = self.TransformerBlock5(P4)
+
+        # 26,26,256 -> 13,13,512
+        P4_downsample = self.down_sample2(P4)
+        # 13,13,512 + 13,13,512 -> 13,13,1024
+        P5 = torch.cat([P4_downsample,P5],axis=1)
+        # 13,13,1024 -> 13,13,512 -> 13,13,1024 -> 13,13,512 -> 13,13,1024 -> 13,13,512
+        P5 = self.make_five_conv4(P5)
+        P5 = self.CBAM6(P5)
+        # P5 = self.TransformerBlock6(P5)
+
+        #---------------------------------------------------#
+        #   第三个特征层
+        #   y3=(batch_size,75,52,52)
+        #---------------------------------------------------#
+        out2 = self.yolo_head3(P3)
+        #---------------------------------------------------#
+        #   第二个特征层
+        #   y2=(batch_size,75,26,26)
+        #---------------------------------------------------#
+        out1 = self.yolo_head2(P4)
+        #---------------------------------------------------#
+        #   第一个特征层
+        #   y1=(batch_size,75,13,13)
+        #---------------------------------------------------#
+        out0 = self.yolo_head1(P5)
+
+        return out0, out1, out2
+
+
+if __name__ == "__main__":
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    m = YoloBody([[6, 7, 8], [3, 4, 5], [0, 1, 2]], 20).to(device)
+    # print(m)
+    summary(m, input_size=(3, 416, 416))
